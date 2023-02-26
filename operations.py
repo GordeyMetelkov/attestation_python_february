45d093b68d178d72d8a971ee149968298e697f59
Gordey <gordeymetelkov@icloud.com>
2023-02-26 14:43:35 +0300

    добавлена функция создания имени файла

--- operations.py
+++ operations.py
@@ -13,3 +13,14 @@ def create_new_note():
         with open ("all_notes.txt", 'a', encoding='utf-8') as file:
             file.write(name + "\n")
 
+def searching_note():
+    name = view.note_name()
+    file_name = name + ".txt"
+    if os.path.isfile(file_name):
+        with open (file_name, 'r', encoding='utf-8') as file:
+            for line in file:
+                print(line.strip())
+    else:
+        print("Такой заметки не существует!")
+        searching_note()
+
--- view.py
+++ view.py
@@ -2,3 +2,7 @@ def note_name():
     return input("Введите название заметки: ")
 def note_text():
     return input("Введите текст заметки: ")
+def f_name():
+    name = note_name()
+    file_name = name + '.txt'
+    return file_name
