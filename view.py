cefaf1d174604f85ca503161fa5eb02dc9a61706
Gordey <gordeymetelkov@icloud.com>
2023-02-26 15:36:06 +0300

    добавлена функция запроса ответа пользователя

--- view.py
+++ view.py
@@ -2,16 +2,20 @@ import operations
 
 def note_name():
     return input("Введите название заметки: ")
+
 def note_text():
     return input("Введите текст заметки: ")
+
 def f_name():
     name = note_name()
     file_name = name + '.txt'
     return file_name
+
 def good_view():
     print()
     operations.show_all_notes()
     print()
+
 def start_view():
     return ("Выберите действие и введите номер, либо 'q', чтобы выйти:\n"
             "1. Создать заметку\n"
@@ -20,3 +24,6 @@ def start_view():
             "4. Добавить информацию в заметку\n"
             "5. Удалить заметку\n"
             "6. Показать список заметок")
+
+def answer():
+    return input("Ответ: ")
